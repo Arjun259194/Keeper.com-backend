9f68912feb19f435a110d715136fc7256744ecb5
arjun <arjun259194@gmail.com>
2023-01-29 23:23:38 +0530

    saving JWT in httponly cookies, added a verify path and a logout path for user

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -4,67 +4,108 @@ import jwt from "jsonwebtoken"
 import UserModel from "../models/User"
 import { errorMessage } from "../modules/functions"
 
-//environment variables
 const JWT_KEY = process.env.JWT_KEY || "topSecret"
 
-export const login = async (request: Request, response: Response) => {
-  try {
-    const { email, password }: { email: string; password: string } = request.body
+//environment variables
+class AuthController {
+  async login(request: Request, response: Response) {
+    try {
+      const { email, password }: { email: string; password: string } =
+        request.body
 
-    const user = await UserModel.findOne({ email: email }).exec()
+      const user = await UserModel.findOne({ email: email }).exec()
 
-    if (!user)
-      return response.status(404).json({
-        status: "email not found",
-        message: "email is not to be found in database",
+      if (!user)
+        return response.status(404).json({
+          status: "email not found",
+          message: "email is not to be found in database",
+        })
+
+      const matchPassword: boolean = await bcrypt.compare(
+        password,
+        user.password
+      )
+
+      if (!matchPassword)
+        return response.status(402).json({
+          status: "Bad request",
+          message: "Password is not current",
+        })
+
+      const TOKEN = jwt.sign(user.id, JWT_KEY)
+
+      return response
+        .cookie("access-token", TOKEN, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+        })
+
+        .status(200)
+        .json({
+          status: "Successful",
+          message: "Authentication successfully done",
+        })
+    } catch (err: any) {
+      errorMessage(err)
+      return response.status(500).json({
+        error: err.message,
       })
+    }
+  }
 
-    const matchPassword: boolean = await bcrypt.compare(password, user.password)
+  async logout(request: Request, response: Response) {
+    return response
+      .clearCookie("access-token")
+      .status(200)
+      .json({ status: "User logged out" })
+  }
 
-    if (!matchPassword)
-      return response.status(402).json({
-        status: "Bad request",
-        message: "Password is not current",
+  async register(request: Request, response: Response) {
+    try {
+      const {
+        name,
+        email,
+        password,
+      }: { name: string; email: string; password: string } = request.body
+
+      if (!password)
+        return response.status(400).json({
+          status: "failed",
+          message: "not valid password",
+        })
+
+      const SALT_ROUND = 10
+      const hashedPassword: string = await bcrypt.hash(password, SALT_ROUND)
+
+      await new UserModel({ name, email, password: hashedPassword }).save()
+
+      return response.status(200).json({
+        status: "ok",
+        message: "user registered",
       })
+    } catch (err: any) {
+      errorMessage(err)
+      return response.status(500).json({
+        error: err.message,
+      })
+    }
+  }
 
-    const TOKEN = jwt.sign(user.id, JWT_KEY)
+  checkAuthorization(request: Request, response: Response) {
+    const token = request.cookies["access-token"]
+    if (!token) {
+      return response.status(401).json({
+        status: "Not authorized",
+      })
+    }
 
-    return response.status(200).json({
-      status: "Successful",
-      message: "Authentication successfully done",
-      token: TOKEN,
-    })
-  } catch (err: any) {
-    errorMessage(err)
-    return response.status(500).json({
-      error: err.message,
-    })
+    try {
+      jwt.verify(token, JWT_KEY)
+      return response.status(200).json({ status: "User is authorized" })
+    } catch (err: any) {
+      return response.status(401).json({ status: "User not authorized" })
+    }
   }
 }
 
-export const register = async (request: Request, response: Response) => {
-  try {
-    const { name, email, password }: { name: string; email: string; password: string } = request.body
-
-    if (!password)
-      return response.status(400).json({
-        status: "failed",
-        message: "not valid password",
-      })
-
-    const SALT_ROUND = 10
-    const hashedPassword: string = await bcrypt.hash(password, SALT_ROUND)
-
-    await new UserModel({ name, email, password: hashedPassword }).save()
-
-    return response.status(200).json({
-      status: "ok",
-      message: "user registered",
-    })
-  } catch (err: any) {
-    errorMessage(err)
-    return response.status(500).json({
-      error: err.message,
-    })
-  }
-}
+export default AuthController
--- src/controllers/task.ts
+++ src/controllers/task.ts
@@ -6,10 +6,16 @@ import Controller from "../modules/classes"
 import { errorMessage } from "./../modules/functions"
 
 class TaskController extends Controller {
-  public Get(request: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, response: Response<any, Record<string, any>>) {
+  public Get(
+    request: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>,
+    response: Response<any, Record<string, any>>
+  ) {
     throw new Error("Method not implemented.")
   }
-  public async Post(request: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, response: Response<any, Record<string, any>>) {
+  public async Post(
+    request: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>,
+    response: Response<any, Record<string, any>>
+  ) {
     try {
       const LIST_ID: string = request.params.listId
       const list = await ListModel.findById(LIST_ID)
@@ -39,35 +45,41 @@ class TaskController extends Controller {
       })
     }
   }
-  public async Put(request: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, response: Response<any, Record<string, any>>) {
+  public async Put(
+    request: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>,
+    response: Response<any, Record<string, any>>
+  ) {
     try {
       const LIST_ID = request.params.listId
 
       const list = await ListModel.findById(LIST_ID).exec()
 
-      if (!list) return response.status(402).json({ status: "bad request", message: "list not found" })
+      if (!list)
+        return response
+          .status(402)
+          .json({ status: "bad request", message: "list not found" })
 
       const TASK_ID = request.params.taskId
 
-      const { state, content, pinned } = request.body
-
       const filter = { _id: LIST_ID, "tasks._id": TASK_ID }
-      const query = {
-        $set: {
-          "tasks.$.state": !state ? null : state,
-          "tasks.$.content": !content ? null : content,
-          "tasks.$.pinned": pinned,
-        },
-      }
-      // const query = {
-      //   $set: {
-      //     "tasks.$.state": !request.body.state ? null : request.body.state,
-      //     "tasks.$.content": !request.body.content ? null : request.body.content,
-      //     "tasks.$.pinned": request.body?.pinned,
-      //   },
-      // }
+      const update: { [key: string]: string | boolean } = {}
 
-      const updatedList = await ListModel.updateOne(filter, query).exec()
+      if (request.body.state !== undefined)
+        update["tasks.$.state"] = request.body.state
+      if (request.body.content !== undefined)
+        update["tasks.$.content"] = request.body.content
+      if (request.body.pinned !== undefined)
+        update["tasks.$.pinned"] = request.body.pinned
+
+      if (Object.keys(update).length === 0)
+        return response.status(402).json({
+          status: "bad request",
+          message: "no valid data sent",
+        })
+
+      const updatedList = await ListModel.updateOne(filter, {
+        $set: update,
+      }).exec()
 
       console.log(updatedList)
 
@@ -83,8 +95,30 @@ class TaskController extends Controller {
       })
     }
   }
-  public Delete(request: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>, response: Response<any, Record<string, any>>) {
-    throw new Error("Method not implemented.")
+  public async Delete(
+    request: Request<ParamsDictionary, any, any, ParsedQs, Record<string, any>>,
+    response: Response<any, Record<string, any>>
+  ) {
+    try {
+      const list = await ListModel.findById(request.params.listId)
+      if (!list)
+        return response
+          .status(404)
+          .json({ status: "not found", message: "list not found" })
+      list.tasks = list.tasks.filter(
+        (task: any) => task.id !== request.params.taskId
+      )
+      await list.save()
+      return response
+        .status(200)
+        .json({ status: "OK", message: "task deleted" })
+    } catch (err: any) {
+      errorMessage(err)
+      return response.json({
+        status: "ERROR",
+        error: err.message,
+      })
+    }
   }
 }
 
